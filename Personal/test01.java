63602fe1e39ddfcf99514b7613d1454d2b504714
sambencheghib <sab219@lehigh.edu>
2015-09-21 21:25:37 +0000

    test01

---
+++ Personal/test01.java
@@ -0,0 +1,30 @@
+//Sam Bencheghib
+//Monday, September 21, 2015
+//This is a practice program
+
+
+//Program purpose: Print out a simple print line. 
+
+
+//Define a class
+public class test01{
+    
+//Add main method
+    public static void main(String[] args) {
+        
+
+//Print statement
+        System.out.println("....................");
+        System.out.println(".    ...    ...    .");
+        System.out.println(".     ^      ^     .");
+        System.out.println(".         |        .");
+        System.out.println(".     _________    .");
+        System.out.println("....................");
+
+        
+//      Save your work before compiling it
+
+        
+    }       //End of main method
+    
+}       //End of class
